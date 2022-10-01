1508c609715e49eb542506816d291774937fa730
Harmanjot Singh <68276852+Harmanjot12@users.noreply.github.com>
2022-09-30 22:58:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    function result(){
+  document.getElementById("marksper").value = 
+  parseFloat(document.getElementById("maths").value) + 
+  parseFloat(document.getElementById("sst").value) + 
+  parseFloat(document.getElementById("science").value) + 
+  parseFloat(document.getElementById("punjabi").value) + 
+  parseFloat(document.getElementById("english").value);
+
+}
